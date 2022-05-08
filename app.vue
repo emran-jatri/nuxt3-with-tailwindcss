7d6ae5d656af8717fe9463075325833315601ad9
ibnshayed <ibnshayed@gmail.com>
2022-05-08 15:33:15 +0600

    first commit

---
+++ app.vue
@@ -0,0 +1,8 @@
+<template>
+    <!-- <NuxtWelcome /> -->
+  <div class="bg-gray-200 h-screen flex justify-center items-center">
+    <div class="p-5 bg-white w-[500px] h-[600px] rounded-2xl shadow-md flex justify-center items-center border-b-8 border-blue-500">
+      <h1 class="text-2xl">Nuxt 3 with Tailwindcss</h1>
+    </div>
+  </div>
+</template>
