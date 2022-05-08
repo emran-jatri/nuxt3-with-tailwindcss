1be81a64766b9649ae227e2c1b88834972f63e6d
ibnshayed <ibnshayed@gmail.com>
2022-05-08 16:33:31 +0600

    login page added

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,12 @@
+
 <template>
     <!-- <NuxtWelcome /> -->
   <div class="bg-gray-200 h-screen flex justify-center items-center">
     <div class="p-5 bg-white w-[500px] h-[600px] rounded-2xl shadow-md flex justify-center items-center border-b-8 border-blue-500">
-      <h1 class="text-2xl">Nuxt 3 with Tailwindcss</h1>
+      <div>
+				<h1 class="text-2xl">Nuxt 3 with Tailwindcss</h1>
+				<NuxtLink to="/login" class="block p-2 mt-4 bg-blue-500 text-white text-center rounded-lg">Login</NuxtLink>
+			</div>
     </div>
   </div>
 </template>
---
+++ pages/login.vue
@@ -0,0 +1,13 @@
+<template>
+    <!-- <NuxtWelcome /> -->
+  <div class="bg-gray-200 h-screen flex justify-center items-center">
+    <div class="p-5 bg-white w-[500px] h-[600px] rounded-2xl shadow-md border-b-8 border-blue-500">
+      <h1 class="text-center text-4xl mb-20 font-bold">Login</h1>
+			<form class="space-y-4">
+				<input class="w-full p-3 outline-none bg-gray-100 rounded-lg focus:border focus:border-blue-500" type="text" placeholder="Enter username">
+				<input class="w-full p-3 outline-none bg-gray-100 rounded-lg focus:border focus:border-blue-500" type="password" placeholder="Enter password">
+				<button class="w-full p-3 bg-blue-500 rounded-lg text-white" type="submit">Submit</button>
+			</form>
+		</div>
+  </div>
+</template>
